2bfd37f869d0e4568810dcb7f2fe0b97af0bab69
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2022-12-04 21:09:12 +0600

    add portfolio link to the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,12 +6,12 @@ export default function About({}: Props) {
   return (
     <div style={{ paddingTop: "100px", maxWidth: "500px" }} className="m-auto ">
       <h2 className="section-header">About</h2>
-      This app demonstrates a typical e-commerce shopping cart. I have used
-      react, typescript, context api, bootstrap and react-bootstrap to develop
-      this application.{" "}
-      {/* <a href="https://portfolio-asif-al-mahmud.onrender.com/#contact">
-        Contact me
-      </a> */}
+      This application demonstrates a typical e-commerce shopping cart. I have
+      used react, typescript, context api, bootstrap and react-bootstrap to
+      develop this application.{" "}
+      <a href="https://portfolio-asif-al-mahmud.onrender.com/">
+        See more projects
+      </a>
     </div>
   );
 }
